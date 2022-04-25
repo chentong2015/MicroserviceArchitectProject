89396be2bc4cbc09d4573ba6fe0e50bba33aad47
tchen <tchen@murex.com>
2022-04-25 17:19:05 +0200

    refresh spring cloud openfeign

--- spring-cloud-feign/src/main/java/com/springcloud/openfeign/demo/JSONPlaceHolderFeignClient.java
+++ spring-cloud-feign/src/main/java/com/springcloud/openfeign/demo/JSONPlaceHolderFeignClient.java
@@ -5,16 +5,17 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-// 通过自定义的配置，提供组件的配置
-// each Feign client is composed of a set of customizable components.
-@FeignClient(value = "jplaceholder",
-        url = "https://localhost:8080/",
-        configuration = MyClientConfiguration.class)
+// (configuration = MyClientConfiguration.class) 通过自定义的配置，提供组件的配置
+// 1. each Feign client is composed of a set of customizable components.
+// 2. For each Feign client, a logger is created by default.
+
+@FeignClient(value = "jplaceholder", url = "https://localhost:8080/")
 public interface JSONPlaceHolderFeignClient {
 
     @RequestMapping(method = RequestMethod.GET, value = "/data")
     String getResponseData();
 
-    @RequestMapping(method = RequestMethod.DELETE, value = "/data/{id}", produces = "application/json")
+    @RequestMapping(method = RequestMethod.DELETE, value = "/data/{id}",
+            produces = "application/json")
     String deleteById(@PathVariable("id") Long id);
 }
