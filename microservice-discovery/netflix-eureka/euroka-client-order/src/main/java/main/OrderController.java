dc476572c29a954dd0dd310e1355b94a4324182c
tchen <chen2015tong@gmail.com>
2025-04-17 16:48:16 +0200

    refresh project

--- microservice-discovery/netflix-eureka/euroka-client-order/src/main/java/main/OrderController.java
+++ microservice-discovery/netflix-eureka/euroka-client-order/src/main/java/main/OrderController.java
@@ -3,7 +3,6 @@ package main;
 import com.netflix.appinfo.InstanceInfo;
 import com.netflix.discovery.EurekaClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
--- microservice-discovery/netflix-eureka/euroka-client-order/src/main/java/main/ServiceOrderApplication.java
+++ microservice-discovery/netflix-eureka/euroka-client-order/src/main/java/main/ServiceOrderApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ServiceOrderApplication {
 
-
     public static void main(String[] args) {
         SpringApplication.run(ServiceOrderApplication.class, args);
     }
